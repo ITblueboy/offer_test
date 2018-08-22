8421d2bf41a2d64a6b2a416e21eca6efebf0a3d3
ITblueboy <18309208067@163.com>
2018-08-23 00:00:09 +0800

    找出旋转数组的最小值

---
+++ min_in_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+	int minNumberInRotateArray(vector<int> rotateArray) {
+		if (rotateArray.size() == 0)
+		{
+			return 0;
+		}
+		sort(rotateArray.begin(), rotateArray.end());
+		return rotateArray[0];
+	}
+};
